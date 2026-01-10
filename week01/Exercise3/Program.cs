516651d9f00d0e78c3294e68370bb3c29d36edf9
onososencross-source <onososensamuel@gmail.com>
2026-01-10 20:48:43 +0000

    Finished exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,8 +2,47 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Stretch: allow replaying the game
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            // Core Requirement 3: generate random magic number (1–100)
+            Random random = new Random();
+            int magicNumber = random.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 0; // Stretch: count guesses
+
+            // Core Requirement 2: loop until guess matches magic number
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            // Stretch: show number of guesses
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            // Stretch: ask to play again
+            Console.Write("Do you want to play again? (yes/no) ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
